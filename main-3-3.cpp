37afad68bbc74fcf4fd8dd58cd14a9fbd0e8db47
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2024-07-27 22:31:25 +0930

    adding questions

--- main-3-3.cpp
+++ main-3-3.cpp
@@ -5,7 +5,7 @@ using namespace std;
 extern double weighted_average(int array[], int n); 
 
 int main () {
-    int array[5] = {1,2,1,4,1,3};
+    int array[5] = {1,2,1,4,1};
     int n = sizeof(array)/sizeof(array[0]);
 
     cout << "the weighted value is: " << weighted_average(array, n) << endl;
