92d8c028b02d40a78f2fc6b2b97cac3276c4565c
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2024-07-27 22:28:29 +0930

    adding questions

--- function-3-5.cpp
+++ function-3-5.cpp
@@ -3,10 +3,8 @@ double sum_even(double array[], int n) {
     
     double sum = 0.0;
     if (n > 1) {
-        for (int i=0; i<n; i++){
-            if(i % 2 == 0) {
-                sum += array[i];
-            }
+        for (int i=0; i<n; i=i+2){
+            sum += array[i];  
         }
     } else { 
         return 0;
