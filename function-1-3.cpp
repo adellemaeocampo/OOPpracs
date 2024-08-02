4d45de8882eab7a7f55e671392f2c72c2725faef
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2024-08-02 21:12:59 +0930

    adding questions from p2

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -12,8 +12,8 @@ void count_digits(int array[4][4]) {
     int eights = 0;
     int nines = 0;
 
-    for(int i=0; i<10; i++){
-        for (int j=0; j<10; j++){
+    for(int i=0; i<4; i++){
+        for (int j=0; j<4; j++){
             switch(array[i][j]){
                 case 0:
                     zeros++;
