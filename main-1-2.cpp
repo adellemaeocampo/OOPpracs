2b39e761459fcb6ef373026b44b54585441ff9bd
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2024-08-08 16:12:15 +0930

    final

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -1,5 +1,5 @@
 
-#include <iostream> 
+// #include <iostream> 
 
 
 extern void modifyArray(double* array, int size, double value);
@@ -7,9 +7,9 @@ extern void modifyArray(double* array, int size, double value);
 int main() {
     double array[5] = {3.0,2.0,1.0,4.0,5.0};
     int size = 5; 
-    int value = 2;
+    double value = 2;
 
-    std::cout << modifyArray(array, size, value) << std::endl;
-    
+    // std::cout << modifyArray(array, size, value) << std::endl;
+    modifyArray(array, size, value);
     return 0;
 }
