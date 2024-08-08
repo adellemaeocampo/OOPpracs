74a3aa46d598321fa773b3926395043ffee962d2
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2024-08-08 16:03:59 +0930

    q2

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,14 +1,14 @@
 
 double arrayMin(double* array, int size) {
-    double min;
+    double *ptr, min;
     min = array[0];
-    
+    ptr = &min;
 
     for(int i=0; i<size; i++) {
         if(array[i] > array[i+1]){
             min = array[i+1];
         }
     }
-    
+
     return min; 
 }
