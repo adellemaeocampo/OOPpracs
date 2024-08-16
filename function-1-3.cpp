db05f99ae21fcecc7b1ffeea46fcfdbf3e41658c
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2024-08-16 20:19:26 +0930

    adding question 3

--- function-1-3.cpp
+++ function-1-3.cpp
@@ -1,5 +1,19 @@
 #include "Person.h"
 
+PersonList createPersonList(int n) {
+    PersonList personlist; 
+    personlist.numPeople = n;
+
+    personlist.people = new Person[n];
+
+    for(int i=0; i<n; i++) {
+        personlist.people[i].name = "Jane Doe";
+        personlist.people[i].age = 1; 
+    }
+
+    return personlist; 
+}
+
 PersonList deepCopyPersonList(PersonList pl) {
     PersonList newpersonlist;
     newpersonlist.numPeople = pl.numPeople; 
