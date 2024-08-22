3d1ea5e333b665c1fcb52249448dac984aab8229
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2024-08-22 16:39:05 +0930

    addiong pe

--- main-1-2.cpp
+++ main-1-2.cpp
@@ -12,6 +12,10 @@ int main() {
     //test constructor 
     AssetPortfolio ap2(5);
     cout << "Asset Portfolio 2: " << ap2.get_num_assets() << endl; 
+    
+    cout << "Does This have assets: " << ap2.has_asset("type1") << endl; 
+
+
 
 
     return 0; 
