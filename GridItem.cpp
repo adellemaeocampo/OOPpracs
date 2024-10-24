bf508c53e6dff44bb6d57d7b3cae053f26e4e380
adellemaeocampo <152781829+adellemaeocampo@users.noreply.github.com>
2024-10-24 16:35:51 +1030

    q2

--- GridItem.cpp
+++ GridItem.cpp
@@ -1,38 +1,37 @@
 
 #include "GridItem.hpp"
 
+int GridItem::activeGridItemCount = 0;
+
 GridItem::GridItem() : x(0), y(0), width(0), height(0) {
-  activeGridItemCount++;
+    activeGridItemCount++;
 }
 
 GridItem::GridItem(int x, int y, int width, int height) : x(x), y(y), width(width), height(height) {
-  activeGridItemCount++;
+    activeGridItemCount++;
 }
 
 GridItem::~GridItem() {
-  activeGridItemCount--;
+    activeGridItemCount--;
 }
 
 void GridItem::setCoordinates(int x, int y) {
-  this->x = x;
-  this->y = y;
+    this->x = x;
+    this->y = y;
 }
 
-std::pair<int, int> GridItem::getCoordinates() {
-  return std::make_pair(x, y);
+std::pair<int, int> GridItem::getCoordinates() const {
+    return std::make_pair(x, y);
 }
 
-int GridItem::getGridWidth() {
-  return width;
+int GridItem::getGridWidth() const {
+    return width;
 }
 
-int GridItem::getGridHeight() {
-  return height;
+int GridItem::getGridHeight() const {
+    return height;
 }
 
 int GridItem::getActiveGridItemCount() {
-  return activeGridItemCount;
+    return activeGridItemCount;
 }
-
-
-int GridItem::activeGridItemCount = 0;
